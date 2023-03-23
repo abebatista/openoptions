8c91a149b0cead889a7a07a32c1b2427e24821a2
Nick Forneris <nicholasdforneris@gmail.com>
2023-03-23 11:16:27 -0500

    fix path

--- server/index.js
+++ server/index.js
@@ -11,14 +11,15 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-// Serve static files from the client/public directory
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
-// Serve the index.html file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'index.html'));
-});
-
+  app.get('*', (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 dotenv.config();
 const limitPromise = limit(120, 60);
