b4ee81f687a3827cf5aed9101f6c04d0fef30e27
Nick Forneris <nicholasdforneris@gmail.com>
2023-03-23 11:50:16 -0500

    path fix

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
   app.get('*', (request, response) => {
     response.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
