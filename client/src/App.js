98589b616e7f906f1a5c77c357892b8baa9798e9
Nick Forneris <nicholasdforneris@gmail.com>
2023-03-24 16:21:39 -0500

    moved the navbar components

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { AgGridReact } from "ag-grid-react";
 import 'ag-grid-community/styles/ag-grid.css'
 import 'ag-grid-community/styles/ag-theme-balham.css'
@@ -12,9 +12,9 @@ const App = () => {
   const [loading, setLoading] = useState(false);
   const [targetValue, setTargetValue] = useState(50);
   const [selectedSymbols, setselectedSymbols] = useState([]);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
   const symbols = ["AAP", "AAPL", "ABBV", "ABT", "ACN", "ADBE", "ADI", "ADM", "ADP", "ADSK", "AEP", "AES", "AFL", "AGNC", "AIG", "AKAM", "ALK", "ALL", "AMAT", "AMD", "AMGN", "AMRN", "AMZN", "ANTM", "APA", "APH", "APTV", "ATVI", "AVGO", "AXP", "BABA", "BAC", "BAX", "BBY", "BEN", "BIDU", "BIIB", "BK", "BKR", "BMY", "BP", "BSX", "BX", "BYND", "C", "CAH", "CAT", "CB", "CCI", "CCL", "CF", "CFG", "CHWY", "CI", "CL", "CLF", "CLX", "CMCSA", "CME", "CNC", "CNP", "COF", "COP", "COST", "CPB", "CPRI", "CRM", "CRON", "CRWD", "CSCO", "CSX", "CTVA", "CVS", "CVX", "CZR", "D", "DAL", "DD", "DE", "DHI", "DHR", "DIS", "DISCA", "DISCK", "DISH", "DKNG", "DLR", "DOCU", "DOW", "DRI", "DVN", "DXC", "EA", "EBAY", "ED", "EIX", "EMR", "EOG", "EQR", "EVRG", "EW", "EXC", "EXPE", "F", "FANG", "FAST", "FCX", "FDX", "FE", "FISV", "FITB", "FOXA", "FSLR", "FTI", "FTV", "GD", "GE", "GILD", "GLW", "GM", "GOLD", "GOOG", "GOOGL", "GPRO", "GPS", "GS", "HAL", "HBAN", "HBI", "HCA", "HD", "HFC", "HIG", "HLT", "HOG", "HOLX", "HON", "HPE", "HPQ", "IBM", "ICE", "INTC", "IP", "IPG", "IRM", "IVZ", "JCI", "JD", "JNJ", "JNPR", "JPM", "K", "KHC", "KMI", "KO", "KR", "KSS", "LEN", "LKQ", "LLY", "LNC", "LOW", "LUMN", "LUV", "LVS", "LYB", "LYFT", "MA", "MAR", "MARA", "MAS", "MCD", "MCHP", "MDLZ", "MDT", "MET", "META", "MGM", "MMM", "MO", "MOS", "MPC", "MRK", "MRNA", "MRO", "MRVL", "MS", "MSFT", "MTB", "MU", "NCLH", "NEE", "NEM", "NET", "NFLX", "NKE", "NLOK", "NOW", "NRG", "NTAP", "NTES", "NVAX", "NVDA", "NWL", "NWSA", "O", "OKE", "OMC", "ORCL", "OXY", "PBCT", "PBR", "PEAK", "PEP", "PFE", "PFG", "PG", "PGR", "PNC", "PPL", "PRU", "PSX", "PXD", "PYPL", "QCOM", "RCL", "RF", "RIG", "ROKU", "ROST", "RTX", "SBUX", "SCHW", "SHOP", "SIRI", "SLB", "SO", "SPG", "SQ", "STX", "SWKS", "SYF", "SYY", "T", "TAP", "TCOM", "TGT", "TJX", "TMO", "TMUS", "TPR", "TRIP", "TRV", "TSLA", "TSM", "TSN", "TT", "TTD", "TTWO", "TXN", "UA", "UAL", "UBER", "ULTA", "UNH", "UNM", "UNP", "UPS", "URBN", "USB", "V", "VFC", "VTR", "VZ", "WAB", "WBA", "WDC", "WFC", "WM", "WMB", "WMT", "WRK", "WU", "WY", "WYNN", "X", "XEL", "XOM", "XRX", "YELP", "ZION", "ZM", "ARKK", "DIA", "EEM", "EFA", "EWJ", "EWW", "EWY", "EWZ", "FXE", "FXI", "GDX", "GLD", "HYG", "IBB", "IWM", "IYR", "KRE", "LQD", "OIH", "QQQ", "SCHD", "SLV", "SMH", "SPXL", "SPXS", "SPY", "SQQQ", "TBT", "TLT", "TQQQ", "UNG", "USO", "UVXY", "VGK", "VXX", "XBI", "XHB", "XLB", "XLC", "XLE", "XLF", "XLI", "XLK", "XLP", "XLRE", "XLU", "XLV", "XLY", "XOP", "XRT"].sort()
-
 
   const dollarUS = Intl.NumberFormat("en-US", {
     style: "currency",
@@ -93,22 +93,31 @@ const App = () => {
     }
   };
 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+
   return (
     <>
       <Container fluid>
         <Navbar bg="dark" sticky="top" className="justify-content-between">
-        <Navbar.Brand>
-              <div className="d-inline-flex align-items-center">
-                <img
-                  src={process.env.PUBLIC_URL + "/OptionsAnalyzerS.png"}
-                  alt="Options Analyzer"
-                  width="35"
-                  height="35"
-                  className="d-inline-block align-top"
-                />
-                <h2 className="m-2">OPEN OPTIONS</h2>
-              </div>
-            </Navbar.Brand>
+          <Navbar.Brand>
+            <div className="d-inline-flex align-items-center">
+              <img
+                src={process.env.PUBLIC_URL + "/OptionsAnalyzerS.png"}
+                alt="Options Analyzer"
+                width="35"
+                height="35"
+                className="d-inline-block align-top m-3"
+              />
+              <h2 className="m-1">OPEN OPTIONS</h2>
+            </div>
+          </Navbar.Brand>
           <Form className="m-3" inline="true" onSubmit={handleSubmit}>
             <InputGroup>
               <InputGroup.Text className="bg-dark text-white">Target ($)</InputGroup.Text>
@@ -128,6 +137,13 @@ const App = () => {
                 Submit
               </Button>
             </InputGroup>
+              <div className="text-center m-2">
+                {selectedSymbols.map((symbol) => (
+                  <Button variant="secondary" key={symbol} style={{ marginRight: "10px" }} disabled>
+                    {symbol}
+                  </Button>
+                ))}
+              </div>
           </Form>
           {/*<Nav className="m-3">
           <Nav.Item>
@@ -150,15 +166,8 @@ const App = () => {
           </Modal.Body>
         </Modal>
         {!loading && (
-          <div className="ag-theme-balham-dark" style={{ height: "82vh", width: "98%", margin: "0 auto" }}>
-                          <div className="text-center mb-2">
-              {selectedSymbols.map((symbol) => (
-                <Button variant="secondary" key={symbol} style={{ marginRight: "10px" }} disabled>
-                  {symbol}
-                </Button>
-              ))}
-            </div>
-            <p className="text-center"><strong>Multisort:</strong> After sorting first column, hold shift while sorting another.</p>
+          <div className="ag-theme-balham-dark" style={{ height: isMobile ? "72vh" : "80vh", width: "98%", margin: "0 auto" }}>
+              <p className="text-left d-inline-block align-middle m-2"><strong>Multisort:</strong> After sorting first column, hold shift while sorting another.</p>
             <AgGridReact
               defaultColDef={defaultColDef}
               columnDefs={columnDefs}
@@ -169,7 +178,13 @@ const App = () => {
             />
           </div>
         )}
-        <div className="mt-5 text-white text-center">Powered by <a className="text-white" href='https://tradier.com' title='Powered by Tradier'>Tradier</a></div>
+        <div className="mt-5 text-white text-center" style={{ display: "flex", alignItems: "flex-end", justifyContent: "center" }}>
+          <span>Powered by</span>
+          <a href='https://tradier.com'>
+            <img src={process.env.PUBLIC_URL + "/tradier_logo.svg"} alt="Options Analyzer" width="45" style={{ marginLeft: "5px", marginBottom: "3px" }} />
+          </a>
+        </div>
+
       </Container>
     </>
   );
